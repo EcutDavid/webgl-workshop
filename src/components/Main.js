0bbd5dd94a5025bcc99b33165664c958f672bdcd
David Guan <davidguandev@gmail.com>
2016-06-29 16:44:09 +0800

    Clean codebase.

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,16 +1,12 @@
-require('normalize.css/normalize.css')
-require('styles/App.scss')
-
+import 'normalize.css/normalize.css'
+import 'styles/App.scss'
 import React from 'react'
-
-let yeomanImage = require('../images/yeoman.png')
 
 class AppComponent extends React.Component {
   render() {
     return (
       <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
-        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
+        <div className="notice">Hello human</div>
       </div>
     )
   }
