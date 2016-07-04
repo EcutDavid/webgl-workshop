6294c7893318f8751fc633696f3384ddb79d6c52
David Guan <davidguandev@gmail.com>
2016-07-04 17:28:44 +0800

    Add foundation and motion-UI

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,18 +1,16 @@
-import 'normalize.css/normalize.css'
-import 'styles/App.scss'
 import React from 'react'
+import 'normalize.css/normalize.css'
+
+import 'styles/app.scss'
 
 class AppComponent extends React.Component {
   render() {
     return (
       <div className="index">
-        <div className="notice">Hello human</div>
+        <button className="button">Hello human</button>
       </div>
     )
   }
-}
-
-AppComponent.defaultProps = {
 }
 
 export default AppComponent
