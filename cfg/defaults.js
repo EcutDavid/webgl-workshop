123efb993142c7fbac70721824133df425773114
David Guan <wguan@splunk.com>
2016-10-13 14:08:56 +0800

    Add missing dependencies.

--- cfg/defaults.js
+++ cfg/defaults.js
@@ -1,7 +1,7 @@
 'use strict'
 const path = require('path')
 const srcPath = path.join(__dirname, '/../src')
-const dfltPort = 8000
+const dfltPort = 8100
 function getDefaultModules() {
   return {
     loaders: [
