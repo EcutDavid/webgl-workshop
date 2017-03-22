0989436330762e1545c454f414e386410b4909ad
David Guan <DavidGuanDev@Gmail.com>
2017-03-22 10:42:46 +0800

    Adding hash chunk in assets

--- cfg/dist.js
+++ cfg/dist.js
@@ -4,6 +4,9 @@ var webpack = require('webpack');
 var baseConfig = require('./base');
 var defaultSettings = require('./defaults');
 
+// Add needed plugins here
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+
 var config = Object.assign({}, baseConfig, {
   entry: path.join(__dirname, '../src/index'),
   cache: false,
@@ -16,7 +19,12 @@ var config = Object.assign({}, baseConfig, {
     new webpack.optimize.UglifyJsPlugin(),
     new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.optimize.AggressiveMergingPlugin(),
-    new webpack.NoErrorsPlugin()
+    new webpack.NoErrorsPlugin(),
+    new HtmlWebpackPlugin({
+      hash: true,
+      filename: '../index.html',
+      template: 'src/index.html.template'
+    })
   ],
   module: defaultSettings.getDefaultModules()
 });
