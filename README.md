301a174cbcb545dad6562d9939e6f018b8b61cd1
David Guan <DavidGuanDev@Gmail.com>
2017-03-22 10:21:45 +0800

    Clean code

--- README.md
+++
@@ -1,6 +0,0 @@
-## static page development template powered by React & Foundation
-
-```
-npm i
-npm start
-```
--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './components/Main';
 
 import 'normalize.css/normalize.css';
 
-import 'styles/App.scss';
+import 'styles/reset.scss';
 
 // Render the main component into the dom
 ReactDOM.render(<App />, document.getElementById('app'));
