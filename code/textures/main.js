35f30daaa6a4b4a2b4484ce1e179af766fcbe0a4
David Guan <davidguandev@gmail.com>
2017-06-22 21:41:40 +0800

    A working texture cube

--- code/textures/main.js
+++ code/textures/main.js
@@ -12,6 +12,76 @@ const canvasHeight = canvasDom.clientHeight;
 gl.clearColor(1, 1, 1, 1);
 gl.clear(gl.COLOR_BUFFER_BIT);
 
+let geometryPoints = [
+  -100.0, -100.0,  100.0,
+   100.0, -100.0,  100.0,
+   100.0,  100.0,  100.0,
+  -100.0,  100.0,  100.0,
+
+  // Back face
+  -100.0, -100.0, -100.0,
+  -100.0,  100.0, -100.0,
+   100.0,  100.0, -100.0,
+   100.0, -100.0, -100.0,
+
+  // Top face
+  -100.0,  100.0, -100.0,
+  -100.0,  100.0,  100.0,
+   100.0,  100.0,  100.0,
+   100.0,  100.0, -100.0,
+
+  // Bottom face
+  -100.0, -100.0, -100.0,
+   100.0, -100.0, -100.0,
+   100.0, -100.0,  100.0,
+  -100.0, -100.0,  100.0,
+
+  // Right face
+   100.0, -100.0, -100.0,
+   100.0,  100.0, -100.0,
+   100.0,  100.0,  100.0,
+   100.0, -100.0,  100.0,
+
+  // Left face
+  -100.0, -100.0, -100.0,
+  -100.0, -100.0,  100.0,
+  -100.0,  100.0,  100.0,
+  -100.0,  100.0, -100.0
+];
+
+const textureCoords = [
+  // Front face
+  0.0, 0.0,
+  1.0, 0.0,
+  1.0, 1.0,
+  0.0, 1.0,
+  // Back face
+  1.0, 0.0,
+  1.0, 1.0,
+  0.0, 1.0,
+  0.0, 0.0,
+  // Top face
+  0.0, 1.0,
+  0.0, 0.0,
+  1.0, 0.0,
+  1.0, 1.0,
+  // Bottom face
+  1.0, 1.0,
+  0.0, 1.0,
+  0.0, 0.0,
+  1.0, 0.0,
+  // Right face
+  1.0, 0.0,
+  1.0, 1.0,
+  0.0, 1.0,
+  0.0, 0.0,
+  // Left face
+  0.0, 0.0,
+  1.0, 0.0,
+  1.0, 1.0,
+  0.0, 1.0,
+];
+
 /*
   * STEP 2
   * Create shaders and link program
@@ -33,30 +103,32 @@ function createShader(gl, type, shaderSource) {
 // vec2 = a vector which length is 2
 const vertexShaderSource = `
   // An attribute will receive data from a buffer
-  attribute vec2 a_color;
-  attribute vec2 position;
-  uniform vec2 resolution;
-  varying vec4 v_color;
-  varying vec4 v_test;
+  attribute vec2 a_cord;
+  attribute vec4 position;
+  uniform vec4 resolution;
+  varying vec2 v_color;
+  varying vec4 v_pos;
+  uniform mat4 transformMat;
 
   void main() {
-    vec2 glSpacePosition = (position / resolution) * 2.0 - 1.0;
+    vec4 transformedPosition = transformMat * position;
+    vec4 glSpacePosition = (transformedPosition / resolution) * 2.0 - 1.0;
 
-    gl_Position = vec4(glSpacePosition * vec2(1, -1), 0, 1);
-    v_color = gl_Position * 0.5 + 0.5 * cos(0.0);
-    v_test = vec4(a_color, 1, 1);
+    gl_Position = vec4(glSpacePosition.xyz * vec3(1, -1, 1), 1);
+    v_pos = position / 60.0 + 0.5;
+    v_color = a_cord;
   }
 `;
 
 const fragmentShaderSource = `
   precision mediump float;
-  varying vec4 v_color;
-  varying vec4 v_test;
+  varying vec2 v_color;
+  varying vec4 v_pos;
   uniform sampler2D u_texture;
+  uniform float lineIndicator;
 
   void main() {
-    // gl_FragColor = vec4(0,0,0,1);
-    // gl_FragColor = vec4(v_color.xy, 1,1);
+    gl_FragColor = v_pos;
     gl_FragColor = texture2D(u_texture, v_color.xy);
   }
 `;
@@ -85,49 +157,43 @@ const program = createProgram(gl, vertexShader, fragmentShader);
   * Configuration
 */
 gl.useProgram(program);
+gl.enable(gl.DEPTH_TEST);
 const positionAttributeLocation = gl.getAttribLocation(program, 'position');
 gl.enableVertexAttribArray(positionAttributeLocation);
-const colorAttributeLocation = gl.getAttribLocation(program, 'a_color');
-gl.enableVertexAttribArray(colorAttributeLocation);
+const cordAttributeLocation = gl.getAttribLocation(program, 'a_cord');
+gl.enableVertexAttribArray(cordAttributeLocation);
+const transformMatUniformLocation = gl.getUniformLocation(program, 'transformMat');
 
 const positionBuffer = gl.createBuffer();
 // In WebGL, we can manipulate many resources on global bind points.
 // Treat bind points as internal global variables hooks inside WebGL.
 gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
 // gl.vertexAttribPointer(location, size, type, normalize, stride, offset)
-gl.vertexAttribPointer(positionAttributeLocation, 2, gl.FLOAT, false, 0, 0);
+gl.vertexAttribPointer(positionAttributeLocation, 3, gl.FLOAT, false, 0, 0);
 
-
-const colorBuffer = gl.createBuffer();
-gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
-gl.vertexAttribPointer(colorAttributeLocation, 2, gl.FLOAT, false, 0, 0);
-gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
-  0, 0,
-  1, 0,
-  0, 1,
-  0, 1,
-  0, 1,
-  0, 1,
-]), gl.STATIC_DRAW);
+const cordBuffer = gl.createBuffer();
+gl.bindBuffer(gl.ARRAY_BUFFER, cordBuffer);
+gl.vertexAttribPointer(cordAttributeLocation, 2, gl.FLOAT, false, 0, 0);
+gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(cordBuffer), gl.STATIC_DRAW);
 const resolutionUniformLocation = gl.getUniformLocation(program, 'resolution');
-gl.uniform2f(resolutionUniformLocation, canvasWidth, canvasHeight);
+const lineIndicatorUniformLocation = gl.getUniformLocation(program, 'lineIndicator');
+gl.uniform4f(resolutionUniformLocation, canvasWidth, canvasHeight, 600, 1);
 
 const texture = gl.createTexture();
 texture.image = new Image();
 texture.image.onload = function() {
   handleLoadedTexture(texture)
 }
-texture.image.src = "github.jpg";
+texture.image.src = "2.jpg";
 
 function handleLoadedTexture(texture) {
   gl.bindTexture(gl.TEXTURE_2D, texture);
-  gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+  // gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
   gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
   gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
   gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
   gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
   gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, texture.image);
-
   draw();
 }
 
@@ -136,28 +202,147 @@ function handleLoadedTexture(texture) {
   * Bind data and call drawArrays
 */
 let angle = 0;
+const cubeVertexIndices = [
+  0, 1, 2,      0, 2, 3,    // Front face
+  4, 5, 6,      4, 6, 7,    // Back face
+  8, 9, 10,     8, 10, 11,  // Top face
+  12, 13, 14,   12, 14, 15, // Bottom face
+  16, 17, 18,   16, 18, 19, // Right face
+  20, 21, 22,   20, 22, 23  // Left face
+];
+const cubeVertexIndexBuffer = gl.createBuffer();
+gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, cubeVertexIndexBuffer);
+gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(cubeVertexIndices), gl.STATIC_DRAW);
 function draw() {
   gl.clear(gl.COLOR_BUFFER_BIT);
-  const pointList = [];
+  const CUBE_WIDTH = 500;
+  const CUBE_HEIGHT = 500;
+  const CUBE_DEPTH = 500;
+  const CUBE_MARGIN = 0;
 
-  pointList.push(0);
-  pointList.push(0);
-  pointList.push(canvasWidth);
-  pointList.push(0);
-  pointList.push(0);
-  pointList.push(canvasHeight);
-
-  pointList.push(canvasWidth);
-  pointList.push(canvasHeight);
-  pointList.push(canvasWidth);
-  pointList.push(0);
-  pointList.push(0);
-  pointList.push(canvasHeight);
-
+  const rotationMatrix = Matrix.xRotate(Matrix.zRotation(angle), angle);
+  gl.uniformMatrix4fv(transformMatUniformLocation, false,
+      Matrix.translate(
+        Matrix.multiply(Matrix.translation(0, 0, 0), rotationMatrix),
+        200, 200, 200
+      )
+  );
+  gl.bindBuffer(gl.ARRAY_BUFFER, cordBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(textureCoords), gl.STATIC_DRAW);
   gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(pointList), gl.STATIC_DRAW);
-  gl.drawArrays(gl.TRIANGLES, 0, pointList.length / 2);
-  // angle += 0.005;
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(geometryPoints), gl.STATIC_DRAW);
+  gl.drawElements(gl.TRIANGLES, 36, gl.UNSIGNED_SHORT, 0);
+  angle += 0.01;
   requestAnimationFrame(draw);
 }
-// requestAnimationFrame(draw);
+
+class Matrix {
+  static identity() {
+    return [
+      1, 0, 0, 0,
+      0, 1, 0, 0,
+      0, 0, 1, 0,
+      0, 0, 0, 1
+    ];
+  }
+
+  static xRotation(angleInRadians) {
+    const c = Math.cos(angleInRadians);
+    const s = Math.sin(angleInRadians);
+
+    return [
+      1, 0, 0, 0,
+      0, c, s, 0,
+      0, -s, c, 0,
+      0, 0, 0, 1,
+    ];
+  }
+
+  static yRotation(angleInRadians) {
+    const c = Math.cos(angleInRadians);
+    const s = Math.sin(angleInRadians);
+
+    return [
+      c, 0, -s, 0,
+      0, 1, 0, 0,
+      s, 0, c, 0,
+      0, 0, 0, 1,
+    ];
+  }
+
+  static zRotation(angleInRadians) {
+    const c = Math.cos(angleInRadians);
+    const s = Math.sin(angleInRadians);
+    return [
+      c, -s, 0, 0,
+      s, c, 0, 0,
+      0, 0, 1, 0,
+      0, 0, 0, 1
+    ];
+  }
+
+  static scaling(sx, sy, sz) {
+    return [
+      sx, 0,  0,  0,
+      0, sy,  0,  0,
+      0,  0, sz,  0,
+      0,  0,  0,  1,
+    ];
+  }
+
+  static translation(tx, ty, tz) {
+    return [
+      1,  0,  0,  0,
+      0,  1,  0,  0,
+      0,  0,  1,  0,
+      tx, ty, tz, 1
+    ];
+  }
+
+  static translate(m, tx, ty, tz) {
+    return Matrix.multiply(m, Matrix.translation(tx, ty, tz));
+  }
+
+  static xRotate(m, angleInRadians) {
+    return Matrix.multiply(m, Matrix.xRotation(angleInRadians));
+  }
+
+  static yRotate(m, angleInRadians) {
+    return Matrix.multiply(m, Matrix.yRotation(angleInRadians));
+  }
+
+  static zRotate(m, angleInRadians) {
+    return Matrix.multiply(m, Matrix.zRotation(angleInRadians));
+  }
+
+  static scale(m, sx, sy, sz) {
+    return Matrix.multiply(m, Matrix.scaling(sx, sy, sz));
+  }
+
+  static perspective(fieldOfViewInRadians, aspect, near, far) {
+    var f = Math.tan(Math.PI * 0.5 - 0.5 * fieldOfViewInRadians);
+    var rangeInv = 1.0 / (near - far);
+
+    return [
+      f / aspect, 0, 0, 0,
+      0, f, 0, 0,
+      0, 0, (near + far) * rangeInv, -1,
+      0, 0, near * far * rangeInv * 2, 0
+    ];
+  }
+
+  static multiply(mat1, mat2) {
+    const length = Math.sqrt(mat1.length);
+    const result = [];
+    for (let i = 0; i < length; i++) {
+      for (let j = 0; j < length; j++) {
+        let value = 0;
+        for (let k = 0; k < length; k++) {
+          value += mat1[i * length + k] * mat2[k * length + j];
+        }
+        result[i * length + j] = value;
+      }
+    }
+    return result;
+  }
+}
